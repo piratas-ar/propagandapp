a4d65a8213f3df2126f535fb21db49bad0b5d704
elsaico <elsaico@partidopirata.com.ar>
2017-04-26 20:51:28 -0300

    Se agregaron validaciones

--- app/models/propaganda.rb
+++ app/models/propaganda.rb
@@ -1,2 +1,17 @@
+require 'uri'
 class Propaganda < ApplicationRecord
+  validates :titulo, presence: true
+  validate :valid_url?
+  def valid_url?
+    return true if self.url.blank?
+    begin
+      uri = URI.parse(self.url)
+      if uri.host.nil?
+        errors.add(:url, "invalida")
+      end
+    rescue URI::InvalidURIError
+      errors.add(:url, "invalida")
+      false
+    end
+  end
 end
